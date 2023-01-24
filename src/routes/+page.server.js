e38fa463a298e2a7abab31872bb35a54702ac76e
Stephen_Lai <stephenlai2021@gmail.com>
2023-01-24 12:35:12 +0800

    commit

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,7 @@ import supabase from '$lib/supabase/config';
 
 export const load = async () => {
 	const { data, error } = await supabase.from('todolist').select('*')
-  // .order('created_at', { ascending: false })
+  .order('created_at', { ascending: false })
 	console.log('data: ', data);
 	return { todolist: data };
 };
